9b915d5a7304c571b59e310cc85028aa8174cdd7
n1mply <140534843+n1mply@users.noreply.github.com>
2024-07-17 11:02:49 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -3,7 +3,7 @@ import asyncio
 
 
 async def get_gemini_response(user_text):
-  genai.configure(api_key="AIzaSyAdcNFbC3OdJIt1eRLNyC5j396pfCDEUoY")
+  genai.configure(api_key="YOUR-KEY")
   model = genai.GenerativeModel(model_name='gemini-pro')
   response = await model.generate_content_async(f"{user_text} Учти, что форматирование текста должно быть согласно форматированию Telegram")
   return response.text
